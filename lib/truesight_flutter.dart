5fcc92e9b83f3a588bb35ea51d3d24ea64398f29
thanhtunguet <thanhtung.uet@gmail.com>
2023-12-17 16:57:50 +0700

    v2312.17.1

--- lib/truesight_flutter.dart
+++ lib/truesight_flutter.dart
@@ -2,13 +2,10 @@ library truesight_flutter;
 
 import 'dart:async';
 
-import 'package:reflectable/reflectable.dart';
-
 import 'truesight_flutter_platform_interface.dart';
 
 export 'package:truesight_core/truesight_core.dart';
 
-part 'go_router.dart';
 part 'server_config.dart';
 
 class TruesightFlutter {
