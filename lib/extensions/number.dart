92d4c62547c068ce49451bef72c89602feae75a0
thanhtunguet <thanhtung.uet@gmail.com>
2024-06-22 09:11:37 +0700

    Fix bugs

--- lib/extensions/number.dart
+++ lib/extensions/number.dart
@@ -10,20 +10,10 @@ extension NumberFormatter on num {
   }
 }
 
-extension DoubleFormatter on double {
-  String asMoney({
-    bool hasDecimals = false,
-    String currency = 'VND',
-  }) {
-    return (this as num).asMoney(hasDecimals: hasDecimals, currency: currency);
-  }
-}
-
 extension IntegerFormatter on double {
   String asMoney({
-    bool hasDecimals = false,
     String currency = 'VND',
   }) {
-    return (this as num).asMoney(hasDecimals: hasDecimals, currency: currency);
+    return (this as num).asMoney(hasDecimals: false, currency: currency);
   }
 }
--- test/truesight_flutter_number.dart
+++ test/truesight_flutter_number.dart
@@ -1,1 +1,14 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:truesight_flutter/truesight_flutter.dart';
 
+void main() {
+  test('number formats', () async {
+    final num n = 1003565.45;
+    final double d = 1005634.4;
+    final int i = 1005634;
+    debugPrint(n.asMoney());
+    debugPrint(d.asMoney());
+    debugPrint(i.asMoney());
+  });
+}
